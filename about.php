c322f89100d0ac69b077fecbafc9efc716d81de9
Tanuja Hegde <108090703+TanujaHegde08@users.noreply.github.com>
2022-11-14 19:47:50 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,32 @@
+<?php include_once "./include/header.php"; ?>
+
+<div class="container">
+    <div class="row">
+        <img src="images/domestic.png" class="col-4 card-img-top" alt="Logo">
+
+        <div class="container col-5" style="margin-top: 40px;">
+            <div class="card">
+                <div class="card-header text-center">
+                    <strong>Domestic Service</strong>
+                </div>
+
+                <div class="card-body">
+
+                <strong class="card-title">Domestic Service Project</strong>
+                <ul>
+                    <li>Tanuja Hegde</li>
+                    <!-- <li>For more enquiries-domesticservices@gmail.com</li> -->
+                    <!-- <li>Demo Member</li> -->
+                </ul>
+            </div>
+
+             <div class="card-footer text-center">
+              domesticservices@helpdesk.com
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include_once "./include/footer.php"; ?>
